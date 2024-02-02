caba033ee13de00e36fe075433dffb16c78516b2
ummaabdul23 <zahrahaani21@gmail.com>
2024-02-02 12:01:56 +0000

    data structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Use the symmetric difference operation to find elements present in only one set
+    # Use the symmetric difference operation to
+    # find elements present in only one set
     diff_set = set_1 ^ set_2
     return diff_set
