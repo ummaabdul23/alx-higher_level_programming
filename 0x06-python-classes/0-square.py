e51b2e5a89fb840146eb5777755afead7d6d8254
ummaabdul23 <zahrahaani21@gmail.com>
2024-02-07 04:12:27 +0000

    python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """
+    class square with attributes:
+        size
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """
+    class square with attribute:
+        size
+        """
 
     def __init__(self, size):
+        """
+        the initialization function for square class
+        """
         self.__size = size
