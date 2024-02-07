f628e5a459c52090cdd19cd32353cd0162f3f72e
ummaabdul23 <zahrahaani21@gmail.com>
2024-02-07 04:08:32 +0000

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """
+    class square with an attribute:
+        size
+    some attributes are protected
+    """
 
     def __init__(self, size=0):
+        """
+        the initialization function for the square class
+        checks for input errors in size
+        """
         if type(size) ==int
             self.__size = size
         elif size < 0:
