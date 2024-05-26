9ed7bd1680417e11d4d69fa31156dc39d4b4661e
ummaabdul23 <zahrahaani21@gmail.com>
2024-05-26 21:12:55 +0000

    mySQL

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- creates table second_table
-CREATE TABLE second_table(id INT AUTO_INCREMENT, name VARCHAR(256), score INT);
+CREATE TABLE second_table (id INT AUTO_INCREMENT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) values (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
