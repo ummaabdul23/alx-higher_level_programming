88b5394c03c368ec917b34ae868627af245d2db9
ummaabdul23 <zahrahaani21@gmail.com>
2024-07-30 22:20:22 +0000

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.alx-tools.com/api/films/1/'.concat(process.argv[2]);
+const starWarsUri = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
 
 request(starWarsUri, function (_err, _res, body) {
   const characters = JSON.parse(body).characters;
